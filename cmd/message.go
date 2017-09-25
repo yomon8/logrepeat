2baf2ba57ac17d5a31b898f699d561406a1cb7eb
yomon8 <sebanngou8@gmail.com>
2017-09-25 17:30:29 +0900

    support http method other than GET
    adjust terminal message

--- cmd/message.go
+++ cmd/message.go
@@ -42,7 +42,7 @@ func printStartMessage() {
 		port)
 	color.Green("Repeat Samples\t:")
 	for i, r := range readreqs[0:5] {
-		fmt.Printf("%s %s\n", color.GreenString(" %d:", i+1), r.URL)
+		fmt.Printf("%s %s\n", color.GreenString(" %d:", i+1), r.String())
 	}
 	fmt.Println("...and more")
 	fmt.Println()
--- parser/alblog_parser.go
+++ parser/alblog_parser.go
@@ -19,9 +19,10 @@ func NewAlbLogParser() *AlbLogParser {
 	a := new(AlbLogParser)
 	a.format = "2006-01-02T15:04:05.000000Z"
 	a.regexp = regexp.MustCompile(
-		`^(.+?) ` + //Method
+		`^(.+?) ` + //Protocol
 			`(\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}.\d{6}Z) ` + //Time
-			`.+? .+? .+? .+? .+? .+? .+? .+? .+? .+? \".+? ` +
+			`.+? .+? .+? .+? .+? .+? .+? .+? .+? .+? \"` +
+			`(.+?) ` + //Method
 			`[^:]+:\/{2,3}[0-9a-z\.\-:\[\]]+?:?[0-9]{0,5}?\/(|.+?) ` + //URL
 			`.+?\" .+? .+? .+? .+? \".+?\"$`)
 	return a
@@ -30,16 +31,17 @@ func NewAlbLogParser() *AlbLogParser {
 // Parse log line
 func (a *AlbLogParser) Parse(line string) (*Entry, error) {
 	matches := a.regexp.FindStringSubmatch(line)
-	if len(matches) < 3 {
+	if len(matches) < 4 {
 		return nil, errors.New("parse error")
 	}
 	entry := new(Entry)
-	entry.Method = matches[1]
+	entry.Protocol = matches[1]
 	var err error
 	entry.DateTime, err = time.Parse(a.format, matches[2])
 	if err != nil {
 		return nil, errors.New("time parse error")
 	}
-	entry.Path = matches[3]
+	entry.Method = matches[3]
+	entry.Path = matches[4]
 	return entry, nil
 }
