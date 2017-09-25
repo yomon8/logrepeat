af75ee051cc339a6d7d8fe063be0eb5490da70f3
yomon8 <sebanngou8@gmail.com>
2017-09-25 19:04:34 +0900

    fix regexp bug

--- parser/alblog_parser.go
+++ parser/alblog_parser.go
@@ -19,11 +19,11 @@ func NewAlbLogParser() *AlbLogParser {
 	a := new(AlbLogParser)
 	a.format = "2006-01-02T15:04:05.000000Z"
 	a.regexp = regexp.MustCompile(
-		`^(.+?) ` + //Protocol
+		`^.+? ` +
 			`(\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}.\d{6}Z) ` + //Time
 			`.+? .+? .+? .+? .+? .+? .+? .+? .+? .+? \"` +
 			`(.+?) ` + //Method
-			`[^:]+:\/{2,3}[0-9a-z\.\-:\[\]]+?:?[0-9]{0,5}?\/(|.+?) ` + //URL
+			`([^:]+):\/{2,3}[0-9a-z\.\-:\[\]]+?:?[0-9]{0,5}?\/(|.+?) ` + //Protocol,URL
 			`.+?\" .+? .+? .+? .+? \".+?\"$`)
 	return a
 }
@@ -35,13 +35,13 @@ func (a *AlbLogParser) Parse(line string) (*Entry, error) {
 		return nil, errors.New("parse error")
 	}
 	entry := new(Entry)
-	entry.Protocol = matches[1]
 	var err error
-	entry.DateTime, err = time.Parse(a.format, matches[2])
+	entry.DateTime, err = time.Parse(a.format, matches[1])
 	if err != nil {
 		return nil, errors.New("time parse error")
 	}
-	entry.Method = matches[3]
+	entry.Method = matches[2]
+	entry.Protocol = matches[3]
 	entry.Path = matches[4]
 	return entry, nil
 }
